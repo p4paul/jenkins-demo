78ca1a087b8543a9b64cab77e715294e2193ecc9
Paul Allen <pallen@perforce.com>
2018-06-06 16:54:46 +0100

    Jenkins updates to fix job `graph-hybrid-demo`

--- jenkins/groovy/default-credentials.groovy
+++ jenkins/groovy/default-credentials.groovy
@@ -3,6 +3,6 @@ import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
 import org.jenkinsci.plugins.p4.credentials.*;
 
-P4PasswordImpl c = new P4PasswordImpl(CredentialsScope.GLOBAL, "p41666", "p41666", "p4.helix:1666", null, "admin", "0", "0", null, "password")
+P4PasswordImpl c = new P4PasswordImpl(CredentialsScope.GLOBAL, "p41666", "p41666", "p4.helix:1666", null, "admin", "0", "0", null, "admin")
 SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
 
