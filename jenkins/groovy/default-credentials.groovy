7dc321d6836b92be3ba7fec811e712f3d9deeaaa
Paul Allen <pallen@perforce.com>
2018-06-11 11:26:42 +0100

    Added Jenkins Git Demo using GCON

--- jenkins/groovy/default-credentials.groovy
+++ jenkins/groovy/default-credentials.groovy
@@ -6,3 +6,5 @@ import org.jenkinsci.plugins.p4.credentials.*;
 P4PasswordImpl c = new P4PasswordImpl(CredentialsScope.GLOBAL, "p41666", "p41666", "p4.helix:1666", null, "admin", "0", "0", null, "admin")
 SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
 
+UsernamePasswordCredentialsImpl u = new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, "gitadmin", "", "admin", "admin")
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), u)
